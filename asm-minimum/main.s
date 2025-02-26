e4f036aac9b5fccce5a6504187bf38de1eebec52
So Koide <sokoide@gmail.cojm>
2025-02-26 18:37:23 +0900

    refactor: simplified asm-minimum

--- asm-minimum/main.s
+++ asm-minimum/main.s
@@ -6,33 +6,22 @@
 	.type	main, @function
 
 main:
-	# setup sp
-	lea.l	mysp, %a0
-	# 0 clear sp@ and decrement sp
-	clr.l	%sp@-
-	#  enter supervisor mode to access the memory block chain
-	DOS		_SUPER
-	move.l	%d0, %sp@
-
+	# push the effective address of .message addr into the stack
 	pea.l	.message1
+
+	# PRINT DOSCALL
 	DOS		_PRINT
+
+	# pop the pushed address
 	addq.l 	#4,%sp
 
-	 DOS		_SUPER
-	 addq.l	#4, %sp
+	# EXIT DOSCALL
+	DOS		_EXIT
 
-	 DOS		_EXIT
-
-	 .section .data
+	.section .data
 	.align	2
 .message1:
 	.string "Hello, world!\r\n"
 
-	# .stack or other custom sections are not supported
-	.section .bss
-	.align	2
-mystack:
-	.ds.l	1024
-mysp:
 	.end 	main
 
