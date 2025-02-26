8f9853e2698f1c33d1d7690c455d0a619c135c3c
So Koide <sokoide@gmail.cojm>
2025-02-26 20:19:57 +0900

    fix: _iocs_b_user

--- vram/main.c
+++ vram/main.c
@@ -1,39 +1,54 @@
 #include "vram.h"
+#include <stdio.h>
 
 #pragma warning disable format
+#include <x68k/dos.h>
 #include <x68k/iocs.h>
 #pragma warning restore format
+
+#define B_SUPER 0x81
 
 int main();
 
 int main() {
-  /* B_SUPER(0); // enter supervisor mode */
-  _iocs_b_super(0); // enter supervisor mode
+    char c;
 
-  // init
-  _iocs_crtmod(10); // 256x256 dots, 256 colors, 2 graphic screens, 2 BG screens
-  _iocs_g_clr_on(); // clear graphics, reset palette to the default, access page 0
-  _iocs_b_curoff(); // stop cursor
-  init_palette();
+    int ssp = _iocs_b_super(0); // enter supervisor mode
 
-  fill_vram(0);
-  fill_vram(1);
+    // init
+    _iocs_crtmod(
+        10); // 256x256 dots, 256 colors, 2 graphic screens, 2 BG screens
+    _iocs_g_clr_on(); // clear graphics, reset palette to the default, access
+                      // page 0
+    _iocs_b_curoff(); // stop cursor
+    init_palette();
 
-  while (1) {
-    // if it's vsync, wait for display period
-    while (!((*mfp) & 0x10))
-      ;
-    // wait for vsync
-    while ((*mfp) & 0x10)
-      ;
+    fill_vram(0);
+    fill_vram(1);
 
-    scroll();
-  }
+    while (1) {
+        // if it's vsync, wait for display period
+        while (!((*mfp) & 0x10))
+            ;
+        // wait for vsync
+        while ((*mfp) & 0x10)
+            ;
 
-  // uninit
-  _iocs_crtmod(16); // 758x512 dots, 16 colors, 1 screen
-  _iocs_g_clr_on(); // clear graphics, reset palette to the default, access page 0
-  _iocs_g_clr_on(); // clear graphics, reset palette to the default, access page 0
-  _iocs_b_super(1); // leave supervisor mode
-  return 0;
+        c = _iocs_b_keysns();
+        if (c) {
+            break;
+        }
+        scroll();
+    }
+
+    // uninit
+    _iocs_b_curon();  // enable cursor
+    _iocs_g_clr_on(); // clear graphics, reset palette to the default, access
+                      // page 0
+    _iocs_crtmod(16); // 758x512 dots, 16 colors, 1 screen
+
+    _iocs_b_super(ssp);
+
+    printf("Key %c pressed\n", c);
+    return 0;
 }
