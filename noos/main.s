0035a428add6d0537dc35eff500c3c2a30a5be26
So Koide <sokoide@gmail.cojm>
2025-03-05 23:54:08 +0900

    add: noos

--- noos/main.s
+++ noos/main.s
@@ -2,17 +2,14 @@
 
 	.section .text
 	.align	2
-	.globl	main
-	.type	main, @function
+	.globl	boot
+	.type	boot, @function
 
 boot:
 	bra.w	entry
 
-	.string "FDD IPL"
-	.even
-
 entry:
-	lea	(mysp, %pc), %a7
+	lea		mysp, %a7
 
 	move.w	#0, %d1
 	move.w	#0, %d2
@@ -20,25 +17,57 @@ entry:
 	IOCS	_B_LOCATE
 	IOCS	_OS_CURON
 
-	lea.l	(.message1, %pc), %a1
+	lea.l	.message1, %a1
 	IOCS	_B_PRINT
 
-	IOCS	_B_KEYINP
-	lea.l	(.message2, %pc), %a1
+	lea.l	.message2, %a1
+	IOCS	_B_PRINT
+
+	# read the sector2
+	#  track 0, side 0, sector 1: IPL
+	#  track 0, side 0, sector 2: <- read from here
+	move.l	#0x010000, %a1
+	move.w	#0x9070, %d1
+	# 03: 2HD
+	# 00: Track 0
+	# 00: Side 0
+	# 02: Sector 2
+	move.l	#0x03000002, %d2
+	move.l	#1024, %d3
+
+	IOCS	_B_READ
+	tst.l	%d0
+	bne.w 	err
+
+done:
+	lea.l	.message_done, %a1
+	IOCS	_B_PRINT
+	jmp		loop
+
+err:
+	move.l	#0x010000, %a2
+	move.l	%d0, (%a2)
+	lea.l	.message_err, %a1
 	IOCS	_B_PRINT
 
 loop:
-	bra	loop
-
+	jmp		loop
 
 	.section .data
-
 .message1:
-	.string	"Press any key\r\n"
+	.string	"Booting Scott & Sandy OS...\r\n"
 	.even
 
 .message2:
-	.string	"Done\r\n"
+	.string	"Loading the disk...\r\n"
+	.even
+
+.message_done:
+	.string	"Sector 2 copied to 0x010000. Ready to go!\r\n"
+	.even
+
+.message_err:
+	.string	"Error\r\n"
 	.even
 
 	.section .bss
@@ -46,4 +75,4 @@ loop:
 mystack:
 	ds.b	256
 mysp:
-	.end entry
+	.end boot
